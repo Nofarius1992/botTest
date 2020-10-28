87db68ff84da39a153d315b2f78940c16f8c6e65
Prozhoga Anatoliy <tolya.prozhoga@gmail.com>
2020-10-28 07:38:35 +0200

    added $response and if, foreach

--- getupdates.php
+++ getupdates.php
@@ -4,5 +4,18 @@
 
 	// ссылка по которой будем стучаться в api
 	$url = 'https://api.telegram.org/bot' . TOKEN . '/getUpdates';
-	echo $url;
+
+	$lastupdate = 138411638;
+
+
+    $response = json_decode(file_get_contents($url), JSON_OBJECT_AS_ARRAY);
+    if($response['ok']) {
+        foreach ($response['result'] as $update) {
+            echo $update['message']['text'];
+            ?>
+            </br>
+            <?php
+        }
+    }
+    var_dump($response);
  ?>
