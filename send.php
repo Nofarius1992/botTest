47611751fd9095444dc66d1dccbd368a0226299b
Prozhoga Anatoliy <tolya.prozhoga@gmail.com>
2020-10-29 07:03:20 +0200

    added file 'send.php'

---
+++ send.php
@@ -0,0 +1,26 @@
+<?php
+
+
+    //405951976
+
+
+    // Токен моего бота
+    const TOKEN = '1376184381:AAHvbWV7rE_EH7dBI3DRe0bWgDjox5TK_N8';
+
+    // ссылка по которой будем стучаться в api
+    $url = 'https://api.telegram.org/bot' . TOKEN . '/sendMessage';
+
+    $params = [
+        'chat_id' => 405951976,
+        'text' => 'Я - бот!'
+    ];
+
+    $url = $url . '?' . http_build_query($params);
+
+
+    $response = json_decode(file_get_contents($url), JSON_OBJECT_AS_ARRAY);
+
+    var_dump($response);
+
+
+?>
